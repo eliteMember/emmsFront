6d7b5ccbb142f01f0bead571dffa71001ee8f7e3
parksy1126 <keilman1126@gmail.com>
2022-03-03 17:03:01 +0900

    로그인 페이지, 로그인 후 분기 처리

--- emms_react/src/App.js
+++ emms_react/src/App.js
@@ -1,17 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Layout/Header.js';
 import Body from './Layout/Body.js';
 import Footer from './Layout/Footer.js';
+import axios from 'axios';
 
 
 function App() {
+
+  const [serverPrefixUrl, serverPrefixUrlChange] = useState('http://localhost:8080');
+  const [isLogin, isLoginChange] = useState(false);
+
+
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/cmmn/getSession')
+      .then(res => {
+        console.log(res);
+        if( res.isLogin === true ){
+          isLoginChange(true);
+        }else{
+          isLoginChange(false);
+        }
+      }).catch(()=>{
+        console.log('세션정보 불러오기 에러');
+      })
+  }, [])
+  
   return (
     <div className="App">
-      <Header/>
-      <Body/>
-      <Footer/>
+      
+      <LoginOrMainControl isLogin = {isLogin}/>
+
     </div>
   );
 }
+
+ function LoginOrMainControl(props){
+  //로그인 후
+  if(props.isLogin === true){
+    return (
+      <>
+      <Header/>
+      <Body/>
+      <Footer/>
+      </>
+    )
+  //로그인 페이지
+  } else if(props.isLogin === false){
+    return (
+      <>
+      <p>로그인 페이지</p>
+      </>
+    )
+  }
+  
+}
+
 
 export default App;
---
+++ emms_react/src/MN/MN100.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './MN100.css';
+function MN100(props){
+    return (
+        <div>
+          <h1>로그인</h1>
+        </div>
+    )
+}
+      
+export default MN100;
