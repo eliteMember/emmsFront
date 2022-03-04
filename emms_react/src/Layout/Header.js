301e0b406399e218c9e925ed2308479efea668f1
eliteMember <gksa2000@naver.com>
2022-03-04 16:38:42 +0900

    헤더메뉴수정

--- emms_react/src/Layout/Header.js
+++ emms_react/src/Layout/Header.js
@@ -1,43 +1,56 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import './Header.css';
-import {Navbar,Container,Nav,NavDropdown} from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Link, Route, Switch} from 'react-router-dom';
+import axios from 'axios'
 
-function Header(props){
+
+function Header(props) {
+  let [menuList, setMenuList] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/menu/getList')
+      .then((rs) => {
+        setMenuList(rs.data);
+      }).catch(() => {
+        setMenuList("메뉴 호출 오류");
+      })
+  }, [])
+
   return (
-    <div className='app-header'>
-      <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">정예멤버</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <div className='Nav-css'>
-              <Nav className="me-auto">
-              <NavDropdown title="팀관리" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="페이지경로">팀관리</NavDropdown.Item>
-                </NavDropdown>
-                <NavDropdown title="프로젝트제안" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="페이지경로">프로젝트기본정보</NavDropdown.Item>
-                  <NavDropdown.Item href="페이지경로">제안공수산정</NavDropdown.Item>
-                  <NavDropdown.Item href="페이지경로">프로젝트견적</NavDropdown.Item>
-                  <NavDropdown.Item href="페이지경로">투입공수산정</NavDropdown.Item>
-                  <NavDropdown.Item href="페이지경로">사전원가산정</NavDropdown.Item>
-                  <NavDropdown.Item href="페이지경로">사전비용계산</NavDropdown.Item>
-                </NavDropdown>
-                <NavDropdown title="프로젝트관리" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="페이지경로">실투입공수관리</NavDropdown.Item>
-                  <NavDropdown.Item href="페이지경로">비용처리</NavDropdown.Item>
-                </NavDropdown>
-                <NavDropdown title="인력관리" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="페이지경로">인력관리</NavDropdown.Item>
-                </NavDropdown>
-                <NavDropdown title="문서관리" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="페이지경로">문서관리</NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-            </div>
-          </Navbar.Collapse>
-      </Navbar>
+    <div>
+      <ul className="nav">
+        <li><Link to="/MAIN" className="home">정예멤버</Link></li>
+        {menuList
+          && menuList.parent.map(
+            (menu) =>
+              <li className="dropdown" key={menu.mnuNum}>
+                <NavBar menu={menu}></NavBar>
+                <NavDrop prtMnuNum={menu.mnuNum}></NavDrop>
+              </li>
+          )
+        }
+      </ul>
     </div>
   )
+
+  function NavBar(props) {
+    return (<Link to="" className="dropbtn">{props.menu.mnuNm}</Link>);
+  }
+
+  
+  function NavDrop(props) {
+    console.log(menuList[props.prtMnuNum]);
+    return (
+      <div className="dropdown-content">
+        {
+          menuList[props.prtMnuNum].map(
+            (menu) => <Link className="menu" to={"/"+menu.mnuUrl} key={menu.mnuNum}>{menu.mnuNm}</Link>
+          )
+        }
+      </div>
+    );
+  }
 }
-    
+
 export default Header;
