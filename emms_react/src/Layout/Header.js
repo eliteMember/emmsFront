e0c5241c843defbda3c059eb58c8bc56e6dc4f70
eliteMember <gksa2000@naver.com>
2022-03-15 09:30:25 +0900

    헤더 및 디자인

--- emms_react/src/Layout/Header.js
+++ emms_react/src/Layout/Header.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import './Header.css';
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import axios from 'axios'
 import { useDispatch, useSelector } from "react-redux";
 import { ACT_USER_INFO_EXPIRE } from "../reducers/userInfo";
@@ -9,6 +9,8 @@ import { ACT_USER_INFO_EXPIRE } from "../reducers/userInfo";
 
 function Header(props) {
   const dispatch = useDispatch();
+
+  const history = useHistory();
 
   const setLogout = () => {
     axios.get('/api/logout')
@@ -20,7 +22,9 @@ function Header(props) {
       })
   };
 
-  let [menuList, setMenuList] = useState(null);
+  const [menuList, setMenuList] = useState(null);
+  
+  const [subMenuList, setSubMenuList] = useState(null);
 
   const { userInfo } = useSelector(state => state.userInfo);
 
@@ -33,42 +37,86 @@ function Header(props) {
       })
   }, [userInfo])
 
-  return (
-    <div>
-      <ul className="nav">
-        <li><Link to="/MAIN" className="home">EMMS</Link></li>
-        {menuList
-          && menuList.parent.map(
-            (menu) =>
-              <li className="dropdown" key={menu.mnuNum}>
-                <NavBar menu={menu}></NavBar>
-                <NavDrop prtMnuNum={menu.mnuNum}></NavDrop>
-              </li>
-          )
-        }
-        <li className="right">{userInfo.usrName}<p> 님</p></li>
-        <div className="vl1"></div>
-        <li><Link to="/MN400" className="myInfo">사용자관리</Link></li>
-        <div className="vl2"></div>
-        <li><a className="logOut" onClick={() => setLogout()}>로그아웃</a></li>
-      </ul>
-    </div>
-  )
-
-  function NavBar(props) {
-    return (<Link to="" className="dropbtn">{props.menu.mnuNm}</Link>);
+  function setSubMenu(menu){
+    console.log(Object.keys(menuList));
+    console.log(Object.keys(menuList).find(key => key === menu.mnuNm));
+    // setSubMenuList(menuList.find(mnu => mnu === menu.mnuNum));
   }
 
+  return (
+    <div className="subPage">
+      <div className="wrapper">
+
+        <div className="header blue">
+          <div className="inner">
+
+            <h1 className="logo"><a onClick={() => history.push('/MAIN')}><img src={require('../imgs/logo_02.png')} alt="회사로고" /><span>정예맴버 프로젝트관리시스템</span></a></h1>
+
+            <nav className="gnbWrap">
+              <ul>
+                {menuList
+                  && menuList.parent.map(
+                    (menu) =>
+                      <li key={menu.mnuNum}
+                        onMouseOver={() => { setSubMenu(menu) }}>
+                        <a onClick={() => history.push('/' + menu.mnuUrl)}><i className={"iconGnb " + menu.mnuNum}></i>{menu.mnuNm}</a>
+                      </li>
+                  )}
+              </ul>
+            </nav>
+
+            <ul className="gnbUtil">
+              <li className="userInfo">
+                <a href="#none" className="ic_position">한국유투더블유</a>
+                <div className="utilSub">
+                  <div className="utilSubMenu">
+                    <div className="utilSubTitle">사용자관리</div>
+                    <ul>
+                      <li><a href="#">- 사용자관리</a></li>
+                      <li><a href="#">- 회사정보</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </li>
+              <li className="userMypage">
+                <a onClick={() => history.push('/MYINFO')} className="name">{userInfo.usrName} 님</a>
+                <div className="utilSub">
+                  <div className="utilSubMenu">
+                    <ul>
+                      <li><a onClick={() => history.push('/MYINFO')}>- 내정보</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </li>
+              <li><a onClick={() => setLogout()} className="ic_logout" title="로그아웃">로그아웃</a></li>
+              <li><a href="#none" className="ic_fullMenu" title="전체메뉴">전체메뉴</a></li>
+            </ul>
+          </div>
+          <div className="tabsArea">
+            <div className='tabs'>
+              <ul>
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div >
+  )
 
   function NavDrop(props) {
     return (
-      <div className="dropdown-content">
-        {
+      <>
+        {menuList
+          &&
           menuList[props.prtMnuNum].map(
-            (menu) => <Link className="menu" to={"/" + menu.mnuUrl} key={menu.mnuNum}>{menu.mnuNm}</Link>
+            (menu) =>
+              <li id={menu.prtMnuNum} onClick={() => history.push('/' + menu.mnuUrl)} key={menu.mnuNum}>
+                <a>{menu.mnuNm}</a>
+              </li>
           )
         }
-      </div>
+      </>
     );
   }
 }
--- emms_react/src/MN/MN100.js
+++ emms_react/src/MN/MN100.js
@@ -52,36 +52,85 @@ function MN100(props) {
   console.log(watch());
 
   return (
-    <div className="wrap">
-      <div className='MN100_DIV'>
-        <form id="loginFrm" name="loginFrm" onSubmit={handleSubmit(onSubmit, onError)}>
-          <h1>로그인</h1>
-          <div>
-            <input type="text" id="loginId" name="loginId" placeholder='아이디'
-              {...register("loginId",
-                {
-                  required: { value: true, message: "아이디를 입력하세요." },
-                  minLength: { value: 3, message: "아이디는 3자리 이상" }
-                }
-              )}
-            />
-            {errors && <p className="valid">{errors?.loginId?.message}</p>}
-            <br />
-            <input type="password" id="password" name="password" placeholder='비밀번호'
-              {...register("password",
-                {
-                  required: { value: true, message: "비밀번호를 입력하세요." }
-                }
-              )}
-            />
-            {errors && <p className="valid">{errors?.password?.message}</p>}
-          </div>
-          <button type="submit">로그인</button>
-          <button><Link to="/MN100_1" className="join_register">회원가입</Link></button>
+    <div className="loginPage">
+      <div className="loginWrap">
+        <section className="loginHeader">
+          <h1><img src={require('../imgs/logo_01.png')} alt="정예맴버로고" /><span>정예맴버 프로젝트관리시스템</span></h1>
+        </section>
+        <section className="loginBody">
+          <div className="loginVisual">
 
-        </form>
+            <div className="slogan">
+              <p className="txt1">안녕하세요</p>
+              <p className="txt2">정예맴버 프로젝트</p>
+              <p className="txt2">관리시스템입니다.</p>
+            </div>
+
+          </div>
+          <div className="loginArea">
+
+            <p className="h2">로그인</p>
+            <div className="hr20 mt10"></div>
+
+            <div className="loginInner">
+              <form id="loginFrm" name="loginFrm" onSubmit={handleSubmit(onSubmit, onError)}>
+                <div className="dFlex inputKeypad id off">
+                  <input type="text" id="loginId" name="loginId" className="w100p" placeholder="아이디" {...register("loginId",
+                    {
+                      required: { value: true, message: "아이디를 입력하세요." },
+                      minLength: { value: 3, message: "아이디는 3자리 이상" }
+                    }
+                  )}
+                  />
+                  <button type="button" className="btnDel"><span>지우기</span></button>
+                </div>
+
+                <div className="dFlex inputKeypad pw on">
+                  <input type="password" id="password" name="password" className="w100p" placeholder="비밀번호" {...register("password",
+                    {
+                      required: { value: true, message: "비밀번호를 입력하세요." }
+                    }
+                  )}
+                  />
+                </div>
+
+                <div className="slogan">
+                  <p className="mt15 bold"><Link to="/MN100_1" className="btnMore2"><span>회원가입</span></Link></p>
+                </div>
+                <div className="mt20 txtC">
+                  <button type="submit" className="btnBig"><span className="ic_login_key">로그인</span></button>
+                </div>
+              </form>
+              <ul className="userLink">
+                <li><a href="#none"><span>아이디찾기</span></a></li>
+                <li><i className="ic_bar"></i></li>
+                <li><a href="#none"><span>비밀번호 재설정</span></a></li>
+              </ul>
+            </div>
+          </div>
+        </section>
       </div>
 
+      <div className="footer">
+        <div className="footer_inner">
+
+          <a href="#none" className="groupWare"><span>그룹웨어 바로가기</span></a>
+          <dl>
+            <dt>TEL</dt>
+            <dd>02-2038-2933</dd>
+          </dl>
+          <div className="ic_dot ml10"></div>
+          <dl className="ml10">
+            <dt>FAX</dt>
+            <dd>070-8282-4283</dd>
+          </dl>
+          <div className="ic_bar ml20"></div>
+          <dl>
+            <dt>한국유투더블유 홈페이지</dt>
+            <dd>www.u2w.co.kr</dd>
+          </dl>
+        </div>
+      </div>
     </div>
   )
 }
