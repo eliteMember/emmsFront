e9ce9585ebad69f1f4da12ac563336fc8caac717
eliteMember <gksa2000@naver.com>
2022-03-22 10:47:19 +0900

    .

--- emms_react/src/PM/PM101/PM101_grid_header.js
+++ emms_react/src/PM/PM101/PM101_grid_header.js
@@ -4,18 +4,28 @@ import React, { useEffect, useState } from 'react';
 
 function PM101GridHeader(props) {
     const [prjList, setPrjList] = useState(null);
+    const [prjStartYm, setPrjStartYm] = useState(null);
+    const [prjEndYm, setPrjEndYm] = useState(null);
+    const [searchEvent, setSearchEvent] = useState(null);
+
     useEffect(() => {
-        axios.get('/api/menu/getList')
-          .then((rs) => {
-            updatePrjList(rs.data);
-          }).catch(() => {
-            updatePrjList("메뉴 호출 오류");
-          })
-      }, [])
-    
-    function updatePrjList(data){
-        setPrjList(data);
+        searchPrj();
+    }, [])
+
+    function searchPrj() {
+        axios.post('/api/pm101/getPrjList', { prjStartYm: prjStartYm, prjEndYm: prjEndYm })
+            .then((rs) => {
+                rs.data && updatePrjList(rs.data);
+                console.log(JSON.stringify(prjList));
+            }).catch(() => {
+                updatePrjList("메뉴 호출 오류");
+            })
     }
+    function updatePrjList(data) {
+        setPrjList(data);
+        setSearchEvent("evt");
+    }
+
     return (
         <>
             <div className="gridUtil">
@@ -32,16 +42,15 @@ function PM101GridHeader(props) {
                                 <tr>
                                     <th scope="row"><span className="tit">프로젝트</span></th>
                                     <td className="txtL">
-                                        <select className="mr20">
-                                            <option>미래에셋생명 비대면업무확대</option>
-                                            <option>NH차세대지방재정 구축</option>
+                                        <select className={"mr20 " + searchEvent}>
+                                            {prjList && prjList.map((prj) => <option key={prj.prjNum}>{prj.prjNm}</option>)}
                                         </select>
                                     </td>
                                     <th scope="row"><span className="tit">프로젝트 기간</span></th>
                                     <td className="txtL" colSpan="3">
-                                        <span className="datepickerBox"><input type="text" placeholder="2021-08-15" /></span>
+                                        <span className="datepickerBox"><input type="text" onChange={(event) => setPrjStartYm(event.target.value)} placeholder="2021-08-15" /></span>
                                         ~
-                                        <span className="datepickerBox"><input type="text" placeholder="2022-11-15" /></span>
+                                        <span className="datepickerBox"><input type="text" onChange={(event) => setPrjEndYm(event.target.value)} placeholder="2022-11-15" /></span>
                                     </td>
                                 </tr>
                             </tbody>
@@ -50,7 +59,7 @@ function PM101GridHeader(props) {
 
                 </div>
                 <div className="fr">
-                    <button type="button" className="btn01"><i className="ic_search"></i><span>조회</span></button>
+                    <button type="button" onClick={()=>searchPrj()} className="btn01"><i className="ic_search"></i><span>조회</span></button>
                 </div>
             </div>
         </>
